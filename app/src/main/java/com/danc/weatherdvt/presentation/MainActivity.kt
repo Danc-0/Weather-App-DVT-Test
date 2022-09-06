0cb21fcad7215144fd78a1fa2abe80fe2dfe0739
Danc-0 <lunjaludaniel@gmail.com>
2022-09-06 10:14:52 +0300

    Fragments set up and app configurations

--- app/src/main/java/com/danc/weatherdvt/presentation/MainActivity.kt
+++ app/src/main/java/com/danc/weatherdvt/presentation/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.danc.weatherdvt.presentation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.danc.weatherdvt.R
+import com.danc.weatherdvt.presentation.fragments.CustomDialog
+import com.danc.weatherdvt.utils.NetworkConnection
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.log
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -11,5 +23,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        transparentSystemBars()
+
+        if (NetworkConnection().checkForInternet(this)){
+            CustomDialog().show(supportFragmentManager, "CustomDialog")
+        }
+
     }
+
+    private fun transparentSystemBars() {
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            statusBarColor = Color.TRANSPARENT
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/danc/weatherdvt/presentation/viewmodels/MainFragViewModel.kt
@@ -0,0 +1,13 @@
+package com.danc.weatherdvt.presentation.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+//@HiltViewModel
+class MainFragViewModel(application: Application): ViewModel() {
+
+
+
+}
